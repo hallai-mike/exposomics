396ef3ce25393aa7f363aaca0b50bb27647b3b5a
IAR <irving.rodriguez973@gmail.com>
2018-01-07 15:00:57 -0800

    add other apps

--- apps/index.client.js
+++ apps/index.client.js
@@ -1,4 +1,7 @@
 import airQuality from './airQuality/client';
 import tempHigh from './tempHigh/client';
+import tempLow from './tempLow/client';
+import precipitation from './precipitation/client';
+import snow from './snow/client';
 
-export default [airQuality, tempHigh];
+export default [airQuality, tempHigh, tempLow, precipitation, snow];
--- apps/index.server.js
+++ apps/index.server.js
@@ -1,4 +1,7 @@
 import airQuality from './airQuality/server';
 import tempHigh from './tempHigh/server';
+import tempLow from './tempLow/server';
+import precipitation from './precipitation/server';
+import snow from './snow/server';
 
-export default { airQuality, tempHigh };
+export default { airQuality, tempHigh, tempLow, precipitation, snow };
